f5dca318c25eea4aa16cfa4d531c18c438ebab42
woojinBot <1403jung@gmail.com>
2023-01-02 14:13:40 +0900

    재현 바보

--- app/src/main/java/com/example/lifeisquest/MainActivity.java
+++ app/src/main/java/com/example/lifeisquest/MainActivity.java
@@ -10,10 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //김무겸 김우진
         //김무겸
-        ///ㅇㄹㄴㄹㅇㄹㄴㅇㅇㄴㄹㄴ란라ㅏㄴ아
         //박상필
         //ㅇㅈㅇ우정저ㅠ유ㅓ유어
-        /ㅇ르란랄을나ㅡㄹㄴ으랑ㄴ란으ㅏ릉라른르ㅏㄹㄴㅇㄹ
+        //ㅇㅈ우저ㅠ유ㅓㅈㅇ뷰ㅓㅇ너즁ㄴ어ㅜ저어쥬유ㅓ유ㅓㅠㅓ
+        //ㅇ르란랄을나ㅡㄹㄴ으랑ㄴ란으ㅏ릉라른르ㅏㄹㄴㅇㄹ
     }
 }
