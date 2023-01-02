3b31e9d9e2316ccc19c987a5bee5ea2153aa37f4
sangpil0104 <psp0104@naver.com>
2023-01-02 13:41:27 +0900

    sangpil

--- app/src/main/java/com/example/lifeisquest/MainActivity.java
+++ app/src/main/java/com/example/lifeisquest/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //ddwddqdqd
+        //상필이의 주석~
     }
 }
