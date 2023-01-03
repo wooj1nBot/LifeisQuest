cba8a969e8188df6fab4936ec98e1c1d8d9d4f1a
kmmugyum <30917465+kmmugyum@users.noreply.github.com>
2023-01-03 16:26:45 +0900

    sign-up 화면, Login 화면 만듦

---
+++ app/src/main/java/com/example/lifeisquest/ForgotPW.java
@@ -0,0 +1,14 @@
+package com.example.lifeisquest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ForgotPW extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_pw);
+    }
+}
--- app/src/main/java/com/example/lifeisquest/LoginActivity2.java
+++ app/src/main/java/com/example/lifeisquest/LoginActivity2.java
@@ -3,6 +3,7 @@ package com.example.lifeisquest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -16,29 +17,40 @@ import com.example.lifeisquest.databinding.ActivityMainBinding;
 
 public class LoginActivity2 extends AppCompatActivity {
     private ActivityLogin2Binding binding;
-    @SuppressLint("SetTextI18n")
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityLogin2Binding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        // 가입하기 btn
         binding.signUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "클릭되었습니다.", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(getApplicationContext(), SignUpActivity2.class);
+                startActivity(intent);
             }
         });
 
+        // forgot pw
+
+
+        // login
         binding.emailEdit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                EditText emailEdit = binding.emailEdit;
-                if(emailEdit.getText().toString().equals("E-mail")) {
-                    Log.d("email_address", emailEdit.getText().toString());
-                    emailEdit.setText("");
-                }
+
             }
         });
+
+        binding.password.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
     }
 }
