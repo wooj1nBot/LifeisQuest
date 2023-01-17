1f47a516d0a681b5154feb66c1155203d221d227
leadjh1118 <117813149+leadjh1118@users.noreply.github.com>
2023-01-18 00:27:17 +0900

    만드는중

---
+++ app/src/main/java/com/example/lifeisquest/Quest_Making_Activity.java
@@ -0,0 +1,13 @@
+package com.example.lifeisquest;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Quest_Making_Activity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.quest_make);
+
+    }
+}
